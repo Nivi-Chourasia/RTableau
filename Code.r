01539b9c672ca588343956cc790101e212c93217
Nivi-Chourasia <51649708+Nivi-Chourasia@users.noreply.github.com>
2019-06-26 11:46:28 -0400

    Update Code.r

--- Code.r
+++ Code.r
@@ -1,4 +1,11 @@
+##RTableau
+##Nchourasia
+##This project gives the output file in .tde format
+
 Install.packages ("devtools")
 devtools :: install_github ("zhiruiwang/RTableau")
 library (RTableau)
 RTableau:::write_tableau(file name, "filename.tde")
+# Be sure you are using 3 colons for write_tableau
+
+ 
